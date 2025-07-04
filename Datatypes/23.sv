7a167437e29187e651013173f971802da839b34b
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-07-04 15:10:09 +0530

    Create 23.sv

---
+++ Datatypes/23.sv
@@ -0,0 +1,15 @@
+module tb;
+	initial begin
+		repeat (5) begin
+			$display ("Repeat this statement");
+		end
+	end
+endmodule
+
+output : 
+# KERNEL: Repeat this statement
+# KERNEL: Repeat this statement
+# KERNEL: Repeat this statement
+# KERNEL: Repeat this statement
+# KERNEL: Repeat this statement
+# KERNEL: Simulation has finished
