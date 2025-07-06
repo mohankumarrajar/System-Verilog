dc8a8ee826dc0a0eeadfda135ffca81b19b48635
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-07-06 15:26:47 +0530

    Create 56.sv

---
+++ Datatypes/56.sv
@@ -0,0 +1,14 @@
+module enum_loop;
+  typedef enum {ZERO, ONE, TWO, THREE} num_t;
+  num_t val;
+  initial begin
+    for (val = THREE; val <= THREE; val++) begin
+      $display("val = %0d", val);
+    end
+  end
+endmodule
+
+output :
+
+# KERNEL: val = 3
+# KERNEL: Simulation has finished.
