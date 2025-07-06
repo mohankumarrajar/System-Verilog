46b785c5d49e425194b3a55d1a306933c4a172a6
MOHANKUMAR R <mohankumareee6105@gmail.com>
2025-07-06 16:02:58 +0530

    Create 82.sv

---
+++ Datatypes/82.sv
@@ -0,0 +1,13 @@
+module m9;
+  initial begin
+    repeat (3) $display("Hello");
+  end
+endmodule
+
+
+output :
+
+# KERNEL: Hello
+# KERNEL: Hello
+# KERNEL: Hello
+# KERNEL: Simulation has finished.
