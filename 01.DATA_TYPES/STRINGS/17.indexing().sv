0b7105b63a701ef902f1e4deeb42b29a0be647b6
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-16 12:25:40 +0530

    Create 17.indexing().sv

---
+++ 01.DATA_TYPES/STRINGS/17.indexing().sv
@@ -0,0 +1,12 @@
+module tb;
+  string s = "VLSI";
+  byte c;
+  initial begin
+    c = s[2]; 
+    $display("Character at index 2 = %s", c);
+  end
+endmodule
+
+output :
+
+Character at index 2 = S
