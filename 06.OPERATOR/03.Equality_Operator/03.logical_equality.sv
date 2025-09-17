a40d6e5f99a78b95042466bd21edcc83398fba6c
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-17 14:38:20 +0530

    Create 03.logical_equality.sv

---
+++ 06.OPERATOR/03.Equality_Operator/03.logical_equality.sv
@@ -0,0 +1,31 @@
+module logical_equality_example;
+  
+  logic [3:0] a, b;
+  
+  initial begin
+    a = 4'b1010;
+    b = 4'b1010;
+    
+    if (a == b) 
+      $display("a == b is TRUE (%b == %b)", a, b);
+    else
+      $display("a == b is FALSE (%b == %b)", a, b);
+      
+    
+    a = 4'b10x0;
+    b = 4'b1010;
+    
+    if (a == b)
+      $display("a == b is TRUE (%b == %b)", a, b);
+    else if (a != b)
+      $display("a == b is FALSE (%b == %b)", a, b);
+    else
+      $display("a == b is UNKNOWN (%b == %b) - contains x/z", a, b);
+  end
+  
+endmodule
+
+output :
+
+a == b is TRUE (1010 == 1010)
+a == b is UNKNOWN (10x0 == 1010) - contains x/z
