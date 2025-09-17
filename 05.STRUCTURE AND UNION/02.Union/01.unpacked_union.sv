2d7d7070932a0de5812b06cee7cec61724bb2166
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-17 12:07:25 +0530

    Update 01.unpacked_union.sv

--- 05.STRUCTURE AND UNION/02.Union/01.unpacked_union.sv
+++ 05.STRUCTURE AND UNION/02.Union/01.unpacked_union.sv
@@ -1,1 +1,22 @@
+module union_type;
+  union {
+    int  x;
+    byte y;
+  } data;
+
+  initial begin
+    data.x = 'hABCF10CD;
+
+    $display("\n x = %0h", data.x);
+    $display("\n y = %0h", data.y);
+
+    data.y = 'h56;
+
+    $display("\n x = %0h", data.x);
+    $display("\n y = %0h", data.y);
+
+    $display("\n data = %p", data);
+    $display("\n size of unpacked union : %0d", $bits(data));
+  end
+endmodule
 
