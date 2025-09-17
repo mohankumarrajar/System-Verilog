52fc4809962feb24b24ed31419f9f409e66d06d2
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-17 12:01:37 +0530

    Update 02.packed_union.sv

--- 05.STRUCTURE AND UNION/02.Union/02.packed_union.sv
+++ 05.STRUCTURE AND UNION/02.Union/02.packed_union.sv
@@ -1,1 +1,26 @@
+typedef union packed {
+  bit [7:0] B;
+  logic [7:0] C;
+} ABC_u;
 
+module unionpacked;
+  ABC_u abc;
+
+  initial begin
+    abc.B = 'hab;
+    $display("\n abc.B = %0h", abc.B);
+
+    abc.C = 'hcd;
+    $display("\n abc.C = %0h", abc.C);
+
+    $display("\n size is: %0d", $bits(ABC_u));
+  end
+endmodule
+
+output :
+
+abc.B = ab
+
+ abc.C = cd
+
+ size is: 8
