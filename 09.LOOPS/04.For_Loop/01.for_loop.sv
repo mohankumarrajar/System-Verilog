67fec2389661bba6dcde0ad492f7e3e0f2649128
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-22 12:44:10 +0530

    Create 01.for_loop.sv

---
+++ 09.LOOPS/04.For_Loop/01.for_loop.sv
@@ -0,0 +1,18 @@
+module for_loop;
+  
+  initial begin
+    
+    for (int i = 0; i < 5;i ++)begin
+      $display("putting the values the positve",i);
+      
+    end
+  end
+endmodule
+
+output :
+
+putting the values the positve          0
+putting the values the positve          1
+putting the values the positve          2
+putting the values the positve          3
+putting the values the positve          4
