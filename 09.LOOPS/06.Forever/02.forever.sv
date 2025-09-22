3dff725a984565f0a2042e73bcd1c5c29896adf9
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-22 15:09:54 +0530

    Create 02.forever.sv

---
+++ 09.LOOPS/06.Forever/02.forever.sv
@@ -0,0 +1,20 @@
+module forever_ex;
+    initial begin
+        forever begin
+          $display("get the solution");
+            #10;
+          break;
+          $display("
+        end
+    end
+  initial begin
+    $dumpfile("fuif.vcd");
+    $dumpvars();
+  end
+  
+endmodule
+
+
+output :
+
+                   get the solution
