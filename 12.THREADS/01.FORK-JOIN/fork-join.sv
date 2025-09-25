92820f66538771d28ffbefc8f1709709044ee812
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-25 14:14:02 +0530

    Create fork-join.sv

---
+++ 12.THREADS/01.FORK-JOIN/fork-join.sv
@@ -0,0 +1,34 @@
+module fork_join;
+  initial begin
+  $display("///////////////fork-join///////////////////");
+  
+  fork
+    
+      begin
+      $display("process is started in 1");
+      #5;
+      $display("process is finished in 1");
+      end
+      
+      begin
+        #2;
+        $display("process is started in 2");
+        #20;
+        $display("process is finished in 2");
+      end
+      
+      join
+      $display("//////////////output///////////////");
+      $finish;
+    end
+    endmodule
+
+
+output :
+
+///////////////fork-join///////////////////
+process is started in 1
+process is started in 2
+process is finished in 1
+process is finished in 2
+//////////////output///////////////
