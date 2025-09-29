6006514a5ab0c30090feec24d2802266a3d16024
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-09-29 10:53:26 +0530

    Update and rename 02.ex.sv to 02.bit_simple_packed.sv

---
+++ 01.DATA_TYPES/2-State/01.Bit/02.bit_simple_packed.sv
@@ -0,0 +1,20 @@
+module bit_ex;
+  bit [7:0]data;
+  
+  initial begin
+    
+    $display("Before print the data = %d",data);
+    
+    data =8'b 11001110;
+    
+    $display("After print the data =%b",data);
+    
+  end
+  
+endmodule
+
+OUTPUT :
+
+Before print the data =   0
+After print the data =11001110
+           V C S   S i m u l a t i o n   R e p o r t 
